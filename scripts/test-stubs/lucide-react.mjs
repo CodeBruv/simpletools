819f65cad13cff07b14ce85bbf8b2e65a9f16501
Code Bruv <abdooolmajeeed@gmail.com>
2026-08-18 16:53:28 +0100

    Refactor lucide-react.mjs [2026-08-18 16:53:28]

--- scripts/test-stubs/lucide-react.mjs
+++ scripts/test-stubs/lucide-react.mjs
@@ -4,13 +4,18 @@
  */
 const icon = (name) => Object.assign(() => null, { displayName: name })
 
+export const Braces = icon('Braces')
 export const Briefcase = icon('Briefcase')
 export const FileArchive = icon('FileArchive')
 export const FileText = icon('FileText')
 export const Image = icon('Image')
 export const ImageDown = icon('ImageDown')
+export const Images = icon('Images')
+export const KeyRound = icon('KeyRound')
+export const Merge = icon('Merge')
 export const Percent = icon('Percent')
 export const PiggyBank = icon('PiggyBank')
 export const QrCode = icon('QrCode')
 export const ReceiptText = icon('ReceiptText')
+export const Scaling = icon('Scaling')
 export const Sparkles = icon('Sparkles')
